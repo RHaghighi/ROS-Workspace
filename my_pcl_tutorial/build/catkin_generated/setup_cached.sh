1b0887ed47db6d76503d422dee30ccdda03adc48
RHaghighi <reza0007@e.ntu.edu.sg>
2017-06-23 15:09:23 +0800

    create

---
+++ my_pcl_tutorial/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/reza/ROS_Workspace/my_pcl_tutorial/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/reza/ROS_Workspace/my_pcl_tutorial/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/reza/ROS_Workspace/my_pcl_tutorial/build/devel/lib:/home/reza/ROS_Workspace/my_pcl_tutorial/build/devel/lib/x86_64-linux-gnu:/home/reza/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/reza/catkin_ws/devel/lib:/opt/ros/indigo/lib:/home/reza/nicp/lib"
+export PATH="/home/reza/ROS_Workspace/my_pcl_tutorial/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/reza/ROS_Workspace/my_pcl_tutorial/build/devel/lib/pkgconfig:/home/reza/ROS_Workspace/my_pcl_tutorial/build/devel/lib/x86_64-linux-gnu/pkgconfig:/home/reza/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/reza/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PYTHONPATH="/home/reza/ROS_Workspace/my_pcl_tutorial/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/reza/ROS_Workspace/my_pcl_tutorial/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/reza/ROS_Workspace/my_pcl_tutorial:/home/reza/catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
---
+++ my_pcl_tutorial/build/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/reza/ROS_Workspace/my_pcl_tutorial/lib/libgtest.pdb"
+  "/home/reza/ROS_Workspace/my_pcl_tutorial/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
